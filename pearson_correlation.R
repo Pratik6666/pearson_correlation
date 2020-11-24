227ce5ad06c695b7723648de0434f4d69070e398
Pratik6666 <ppchowdhury6@gmail.com>
2020-11-24 16:44:19 +0530

    pearson correlation

--- pearson_correlation.R
+++ pearson_correlation.R
@@ -5,3 +5,10 @@ plot(weight, main = "chocolate vs  Weight", xlab = "Chocolate (grams)",
      ylab = "Weight (kgs)")
 names(weight) <- c("Chocolate (x)","Weight (kgs)")
 names(weight)
+## conf.level cab be adjusted
+cor.test(weight$`Chocolate (x)`,weight$`Weight (kgs)`,conf.level = 0.99)
+## alternative can be set to "greater" or "two.sided"
+cor.test(weight$`Chocolate (x)`,weight$`Weight (kgs)`,alternative = "greater")
+## regression line
+details<-lm(weight$`Weight (kgs)`~ weight$`Chocolate (x)`)
+abline(details, col="red")
